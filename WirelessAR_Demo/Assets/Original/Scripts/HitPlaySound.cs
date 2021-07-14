9d93d0119de7fe972994de375ead988ce82b1223
KawaSwitch <bb52120306@ms.nagasaki-u.ac.jp>
2021-07-14 17:57:47 +0900

    Add hit sound when a player hits obstacles
    Add a util script that helps play sound on collision
    Damage wav file could be replaced with somothing else later

---
+++ WirelessAR_Demo/Assets/Original/Scripts/HitPlaySound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HitPlaySound : MonoBehaviour {
+
+    public AudioClip sound;
+    public float volume;
+
+    void OnCollisionEnter(Collision collision) {
+        AudioSource.PlayClipAtPoint(sound, transform.position, volume);
+    }
+}
